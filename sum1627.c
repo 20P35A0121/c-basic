a26d62840d2f196bbd1a385a01b8f4c5b784fab8
20P35A0121 <93193600+20P35A0121@users.noreply.github.com>
2021-10-29 23:26:52 -0700

    Add files via upload

---
+++ sum1627.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	// variables
+	int num1, num2,sum;
+	 
+	//initialzation
+	num1= 813;
+	num2= 814;
+	
+	//calculation
+	sum = num1+num2;
+	
+	//output
+	printf("sum of %d and %d is %d",num1,num2,sum);
+}
